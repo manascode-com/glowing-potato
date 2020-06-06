cae5f2f17c6996dd07474240cd3842e02752cca1
Manas <imanaspaul@gmail.com>
2020-06-06 21:41:26 +0530

    User profile created

--- main/settings.py
+++ main/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'userprofile',
 ]
 
 MIDDLEWARE = [
@@ -90,6 +92,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'userprofile.User'
 
 
 # Internationalization
